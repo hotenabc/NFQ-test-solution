1b9206b3bb033774f142b22a13edf45ba07eb324
hotenabc <hotenabc789@gmail.com>
2024-08-29 00:26:30 +0700

    Update texttest_fixture.php

--- fixtures/texttest_fixture.php
+++ fixtures/texttest_fixture.php
@@ -13,13 +13,13 @@ $items = [
     new Item('+5 Dexterity Vest', 10, 20),
     new Item('Aged Brie', 2, 0),
     new Item('Elixir of the Mongoose', 5, 7),
-    new Item('Sulfuras, Hand of Ragnaros', 0, 80),
-    new Item('Sulfuras, Hand of Ragnaros', -1, 80),
-    new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20),
-    new Item('Backstage passes to a TAFKAL80ETC concert', 10, 49),
-    new Item('Backstage passes to a TAFKAL80ETC concert', 5, 49),
+    new Item('Sulfuras', 0, 80),
+    new Item('Sulfuras', -1, 80),
+    new Item('Backstage passes', 15, 20),
+    new Item('Backstage passes', 10, 49),
+    new Item('Backstage passes', 5, 49),
     // this conjured item does not work properly yet
-    new Item('Conjured Mana Cake', 3, 6),
+    new Item('Conjured', 3, 6),
 ];
 
 $app = new Product($items);
