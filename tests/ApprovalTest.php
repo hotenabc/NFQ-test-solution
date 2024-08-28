ecb3b398aa3109d45d0d7d8b3ac12c21c0dcee08
hotenabc <hotenabc789@gmail.com>
2024-08-28 16:23:34 +0700

    Update ApprovalTest.php

--- tests/ApprovalTest.php
+++ tests/ApprovalTest.php
@@ -5,8 +5,8 @@ declare(strict_types=1);
 namespace Tests;
 
 use ApprovalTests\Approvals;
-use GildedRose\GildedRose;
-use GildedRose\Item;
+use Product\Product;
+use Product\Item;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -23,7 +23,7 @@ class ApprovalTest extends TestCase
     public function testFoo(): void
     {
         $items = [new Item('foo', 0, 0)];
-        $app = new GildedRose($items);
+        $app = new Product($items);
         $app->updateQuality();
 
         Approvals::verifyList($items);
