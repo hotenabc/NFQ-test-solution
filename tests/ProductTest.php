7bd6044db17d9da2ad12d646a80cca3592d6b97b
hotenabc <hotenabc789@gmail.com>
2024-08-29 10:49:05 +0700

    Update ProductTest.php

--- tests/ProductTest.php
+++ tests/ProductTest.php
@@ -4,17 +4,17 @@ declare(strict_types=1);
 
 namespace Tests;
 
-use GildedRose\GildedRose;
-use GildedRose\Item;
+use Product\Product;
+use Product\Item;
 use PHPUnit\Framework\TestCase;
 
-class GildedRoseTest extends TestCase
+class ProductTest extends TestCase
 {
     public function testFoo(): void
     {
         $items = [new Item('foo', 0, 0)];
-        $gildedRose = new GildedRose($items);
-        $gildedRose->updateQuality();
+        $product = new Product($items);
+        $product->updateQuality();
         $this->assertSame('fixme', $items[0]->name);
     }
 }
