cb125ffdda01e3298f3e7e904d6c81bf960a4656
hotenabc <hotenabc789@gmail.com>
2024-08-29 09:50:13 +0700

    Add files via upload

--- fixtures/texttest_fixture.php
+++ fixtures/texttest_fixture.php
@@ -4,25 +4,25 @@ declare(strict_types=1);
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-use Product\Product;
-use Product\Item;
+use GildedRose\GildedRose;
+use GildedRose\Item;
 
-echo 'NFQ-GREAT!' . PHP_EOL;
+echo 'OMGHAI!' . PHP_EOL;
 
 $items = [
     new Item('+5 Dexterity Vest', 10, 20),
     new Item('Aged Brie', 2, 0),
     new Item('Elixir of the Mongoose', 5, 7),
-    new Item('Sulfuras', 0, 80),
-    new Item('Sulfuras', -1, 80),
-    new Item('Backstage passes', 15, 20),
-    new Item('Backstage passes', 10, 49),
-    new Item('Backstage passes', 5, 49),
+    new Item('Sulfuras, Hand of Ragnaros', 0, 80),
+    new Item('Sulfuras, Hand of Ragnaros', -1, 80),
+    new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20),
+    new Item('Backstage passes to a TAFKAL80ETC concert', 10, 49),
+    new Item('Backstage passes to a TAFKAL80ETC concert', 5, 49),
     // this conjured item does not work properly yet
-    new Item('Conjured', 3, 6),
+    new Item('Conjured Mana Cake', 3, 6),
 ];
 
-$app = new Product($items);
+$app = new GildedRose($items);
 
 $days = 2;
 if ((is_countable($argv) ? count($argv) : 0) > 1) {
