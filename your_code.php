1679be4984cf6d09e97659815498f814d2756e98
hotenabc <hotenabc789@gmail.com>
2024-08-30 14:50:25 +0700

    Update your_code.php

--- your_code.php
+++ your_code.php
@@ -4,6 +4,21 @@ declare(strict_types=1);
 
 namespace Product;
 
+class Item implements \Stringable
+{
+    public function __construct(
+        public string $name,
+        public int $sellIn,
+        public int $quality
+    ) {
+    }
+
+    public function __toString(): string
+    {
+        return (string) "{$this->name}, {$this->sellIn}, {$this->quality}";
+    }
+}
+
 final class Product
 {
     /**
