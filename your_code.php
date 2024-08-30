9e3237ab956930e61e839db37175b2b6aaf0a771
hotenabc <hotenabc789@gmail.com>
2024-08-30 14:48:41 +0700

    Update your_code.php

--- your_code.php
+++ your_code.php
@@ -65,3 +65,35 @@ final class Product
         }
     }
 }
+
+echo 'NFQ-GREAT!' . PHP_EOL;
+
+$items = [
+    new Item('+5 Dexterity Vest', 10, 20),
+    new Item('Aged Brie', 2, 0),
+    new Item('Elixir of the Mongoose', 5, 7),
+    new Item('Sulfuras', 0, 80),
+    new Item('Sulfuras', -1, 80),
+    new Item('Backstage passes', 15, 20),
+    new Item('Backstage passes', 10, 49),
+    new Item('Backstage passes', 5, 49),
+    // this conjured item does not work properly yet
+    new Item('Conjured', 3, 6),
+];
+
+$app = new Product($items);
+
+$days = 7;
+if ((is_countable($argv) ? count($argv) : 0) > 1) {
+    $days = (int) $argv[1];
+}
+
+for ($i = 0; $i < $days; $i++) {
+    echo "-------- day {$i} --------" . PHP_EOL;
+    echo 'name, sellIn, quality' . PHP_EOL;
+    foreach ($items as $item) {
+        echo $item . PHP_EOL;
+    }
+    echo PHP_EOL;
+    $app->updateQuality();
+}
